9e4952728cd81d622eba765db74477568a693565
Ahmad Kaddoura <kaddoura.ahmad@gmail.com>
2024-01-18 11:54:10 -0800

    Initial Commit

---
+++ navigationproject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  navigationproject
+//
+//  Created by ahmad kaddoura on 1/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ navigationproject/navigationprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationprojectApp.swift
+//  navigationproject
+//
+//  Created by ahmad kaddoura on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct navigationprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
